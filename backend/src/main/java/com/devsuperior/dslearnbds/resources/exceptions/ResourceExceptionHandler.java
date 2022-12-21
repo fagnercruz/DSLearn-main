50401f863e9bcae32838a29cf610a2d610030b63
Fagner Cruz <fagner@sti.ufpb.br>
2022-12-21 13:22:07 -0300

    User by id

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -17,49 +19,54 @@ import com.devsuperior.dslearnbds.service.exceptions.ResourceNotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	// Para logs
+	private static Logger logger = LoggerFactory.getLogger(ResourceExceptionHandler.class);
+
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest requerst){
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest requerst) {
 		StandardError err = new StandardError();
-		HttpStatus status = HttpStatus.NOT_FOUND;
-		
+		HttpStatus status = HttpStatus.CONFLICT;
+
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Recurso não encontrado (Resource not found)");
 		err.setMessage(e.getMessage());
 		err.setPath(requerst.getRequestURI());
 		
+		logger.error(err.getMessage() + " - " + err.getError());
+		
 		return ResponseEntity.status(status).body(err);
 	}
-	
+
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest requerst){
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest requerst) {
 		StandardError err = new StandardError();
 		HttpStatus status = HttpStatus.BAD_REQUEST;
-		
+
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Database Exception");
 		err.setMessage(e.getMessage());
 		err.setPath(requerst.getRequestURI());
-		
+
 		return ResponseEntity.status(status).body(err);
 	}
-	
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest requerst){
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest requerst) {
 		ValidationError err = new ValidationError();
 		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
-		
+
 		err.setTimestamp(Instant.now());
 		err.setStatus(status.value());
 		err.setError("Validation Exception");
 		err.setMessage(e.getMessage());
 		err.setPath(requerst.getRequestURI());
-		
-		for(FieldError f : e.getBindingResult().getFieldErrors()) {
+
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
 			err.addError(f.getField(), f.getDefaultMessage());
 		}
-		
+
 		return ResponseEntity.status(status).body(err);
 	}
 }
