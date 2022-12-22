c214e2050960dd19ba2e2d268faae0b184850f4f
Fagner Cruz <fagner@sti.ufpb.br>
2022-12-22 07:41:10 -0300

    Refresh token

--- backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/config/AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -43,6 +44,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private JwtTokenEnhancer enhancer;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
@@ -55,8 +59,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient(clientId)
 			.secret(passwordEncoder.encode(clientSecret))
 			.scopes("read","write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(jwtDuration); // 24hs em segundos
+			.authorizedGrantTypes("password","refresh_token")
+			.accessTokenValiditySeconds(jwtDuration) // 24hs em segundos
+			.refreshTokenValiditySeconds(jwtDuration);
 	
 	}
 
@@ -68,7 +73,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints.authenticationManager(authenticationManager)
 			.tokenStore(jwtTokenStore)
 			.accessTokenConverter(accessTokenConverter)
-			.tokenEnhancer(chain);
+			.tokenEnhancer(chain)
+			.userDetailsService(userDetailsService);
 		
 	}
 
